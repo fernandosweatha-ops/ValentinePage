ef97ca8219d61f7e91c8b876a269af9d71ea6dbe
fernandosweatha-ops <fernandosweatha@gmail.com>
2026-02-04 18:50:16 +0530

    Add Valentine’s webpage files
    Commit directly to the main branch

---
+++ index.html2/ValatinesPage/Script.js
@@ -0,0 +1,4 @@
+function cutCake() {
+  document.getElementById("cake").style.opacity = "0.5"; // cake looks "cut"
+  document.getElementById("message").style.display = "block"; // reveal message
+}
